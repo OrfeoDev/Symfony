43bc88b7250549781e6f083d5cd7985dfc388010
OrfeoDev <orfe34@icloud.com>
2022-02-11 13:28:11 +0100

    EXO CORRECTION modification du password Utilisateur

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\VoitureRepository;
 use App\Security\AppCustomAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -46,4 +48,46 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+
+    // EXO CORRECTION modification du password Utilisateur
+
+    #[Route('/changePassword/{id}', name: 'changePassword')]
+#[IsGranted('ROLES_USER')]
+    public function changePassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, User $user, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    {
+$userCurrentId =$this->getUser()->getId();
+
+if ($userCurrentId !==$user->getId()&&!$this->isGranted('ROLE_ADMIN')){
+
+    return $this->redirectToRoute('changePassWord',['id'=>$userCurrentId]);
+}
+        $form = $this->createForm(changePassword::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+        }
+
+        return $this->render('registration/changePassword.html.twig', [
+            'changePassword' => $form->createView(),
+        ]);
+    }
+
+
 }
